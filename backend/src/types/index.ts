0ab8a418c496e2a39040ce26175f9d82b37e8fbc
Jaohar Raihan <jaoharraihan@Jaohars-MacBook-Pro.local>
2026-09-11 04:18:15 +0600

    fix: declare bcrypt types and move @types to dependencies for production builds

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -17,3 +17,9 @@ declare module 'fastify' {
     ) => Promise<void>
   }
 }
+
+declare module 'bcrypt' {
+  export function hash(data: string | Buffer, saltOrRounds: string | number): Promise<string>
+  export function compare(data: string | Buffer, encrypted: string): Promise<boolean>
+  export function genSalt(rounds?: number): Promise<string>
+}
